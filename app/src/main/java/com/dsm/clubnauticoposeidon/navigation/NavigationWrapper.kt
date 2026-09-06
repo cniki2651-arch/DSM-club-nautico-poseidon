d2c1ad6022e72c276ff01d6b5a46072bc0e33786
cniki2651-arch <cniki2651@gmail.com>
2026-09-06 00:48:35 -0500

    añadido NavigationWrapper y completado Initial

---
+++ app/src/main/java/com/dsm/clubnauticoposeidon/navigation/NavigationWrapper.kt
@@ -0,0 +1,39 @@
+package com.dsm.clubnauticoposeidon.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.dsm.clubnauticoposeidon.ui.screens.initial.InitialScreen
+import com.dsm.clubnauticoposeidon.ui.screens.login.LoginScreen
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun NavigationWrapper(
+    navHostController: NavHostController,
+    auth: FirebaseAuth
+) {
+    NavHost(navController = navHostController, startDestination = "initial") {
+        composable("initial") {
+            InitialScreen(
+                onLogin = { navHostController.navigate("logIn") },
+                onSignUp = { navHostController.navigate("signUp") }
+            )
+        }
+
+        composable("logIn") {
+            LoginScreen(
+                auth = auth,
+                onSignUp = { navHostController.navigate("signUp") }
+            )
+        }
+
+        composable("signUp") {
+            // SignUpScreen(auth, onLogin = { navHostController.navigate("logIn") })
+        }
+
+        composable("home") {
+            // HomeScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/dsm/clubnauticoposeidon/ui/screens/login/LoginScreen.kt
@@ -0,0 +1,13 @@
+package com.dsm.clubnauticoposeidon.ui.screens.login
+
+
+import androidx.compose.runtime.Composable
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun LoginScreen(
+    auth: FirebaseAuth,
+    onSignUp: () -> Unit = {}
+) {
+    // TODO: acá va el diseño del login (campos de email, contraseña, etc.)
+}
