f20e7422b48459ae2a2b595b66d8144f41525844
cniki2651-arch <cniki2651@gmail.com>
2026-09-14 20:39:50 -0500

    actualizacion de NavigationWrapper

--- app/src/main/java/com/dsm/clubnauticoposeidon/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/dsm/clubnauticoposeidon/navigation/NavigationWrapper.kt
@@ -1,11 +1,13 @@
 package com.dsm.clubnauticoposeidon.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.dsm.clubnauticoposeidon.ui.screens.initial.InitialScreen
 import com.dsm.clubnauticoposeidon.ui.screens.login.LoginScreen
+import com.dsm.clubnauticoposeidon.ui.screens.recovery.RecuperarPasswordScreen
 import com.dsm.clubnauticoposeidon.ui.screens.signup.SignUpScreen
 import com.dsm.clubnauticoposeidon.ui.screens.home.HomeScreen
 import com.google.firebase.auth.FirebaseAuth
@@ -21,28 +23,33 @@ fun NavigationWrapper(
     NavHost(navController = navHostController, startDestination = startDest) {
         composable("initial") {
             InitialScreen(
-                onLogin = { navHostController.navigate("logIn") },
-                onSignUp = { navHostController.navigate("signUp") }
+                onLogin = { navHostController.navigate("login") },
+                onSignUp = { navHostController.navigate("signup") }
             )
         }
 
-         composable("login") {
-                    LoginScreen(
-                        auth = auth,
-                        onSignUp = { navController.navigate("signup") },
-                        onLoginSuccess = { navController.navigate("home") },
-                        onBackClick = { navController.popBackStack() },
-                        onForgotPassword = { navController.navigate("recuperar_password") }
-                    )
-                }
-
+        composable("login") {
+            LoginScreen(
+                auth = auth,
+                onSignUp = { navHostController.navigate("signup") },
+                onLoginSuccess = { navHostController.navigate("home") },
+                onBackClick = { navHostController.popBackStack() },
+                onForgotPassword = { navHostController.navigate("recuperar_password") }
+            )
         }
 
-        composable("signUp") {
+        composable("signup") {
             SignUpScreen(
                 auth = auth,
-                onLogin = { navHostController.navigate("logIn") },
+                onLogin = { navHostController.navigate("login") },
                 onBackClick = { navHostController.popBackStack() }
+            )
+        }
+
+        composable("recuperar_password") {
+            RecuperarPasswordScreen(
+                viewModel = viewModel(),
+                onVolver = { navHostController.popBackStack() }
             )
         }
 
@@ -50,7 +57,7 @@ fun NavigationWrapper(
             HomeScreen(
                 auth = auth,
                 onLogout = {
-                    navHostController.navigate("logIn") {
+                    navHostController.navigate("login") {
                         popUpTo(0) // Limpiar la pila de navegación al salir
                     }
                 }
---
+++ app/src/main/java/com/dsm/clubnauticoposeidon/ui/screens/recovery/RecuperarPasswordScreen.kt
@@ -0,0 +1,2 @@
+package com.dsm.clubnauticoposeidon.ui.screens.recovery
+
